0eb8b6a5012f56175fdc112ea4e9feb8eaa454cc
mcraig2342 <matthewcookecraig10@gmail.com>
2021-02-07 12:30:20 -0700

    refactor resraurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -15,26 +15,26 @@ function addMenuItem(restaurantType, newItem) {
   if (menuType === "lunch") {
     for (var i = 0; i < restaurant.menus.lunch.length; i++) {
       if (newItem === restaurant.menus.lunch[i]) {
-        return restaurant
+        return restaurant;
       }
     }
     restaurant.menus.lunch.push(newItem);
   } else if (menuType === "breakfast") {
     for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
       if (newItem === restaurant.menus.breakfast[i]) {
-        return restaurant
+        return restaurant;
       }
     }
     restaurant.menus.breakfast.push(newItem);
   } else {
     for (var i = 0; i < restaurant.menus.dinner.length; i++) {
       if (newItem === restaurant.menus.dinner[i]) {
-        return restaurant
+        return restaurant;
       }
     }
     restaurant.menus.dinner.push(newItem);
     }
-    return restaurant
+    return restaurant;
 }
 
 function removeMenuItem(restaurantType, item, menuType) {
@@ -42,26 +42,26 @@ function removeMenuItem(restaurantType, item, menuType) {
     for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
       if (item === restaurant.menus.breakfast[i].name) {
         restaurant.menus.breakfast.splice(i, 1);
-        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`
+        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`;
       }
     }
-    return `Sorry, we don't sell ${item}, try adding a new recipe!`
+    return `Sorry, we don't sell ${item}, try adding a new recipe!`;
   } else if (menuType === "lunch"){
     for (var i = 0; i < restaurant.menus.lunch.length; i++) {
       if (item === restaurant.menus.lunch[i].name) {
         restaurant.menus.lunch.splice(i, 1);
-        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`
+        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`;
       }
     }
-    return `Sorry, we don't sell ${item}, try adding a new recipe!`
+    return `Sorry, we don't sell ${item}, try adding a new recipe!`;
   } else {
     for (var i = 0; i < restaurant.menus.dinner.length; i++) {
       if (item === restaurant.menus.dinner[i].name) {
         restaurant.menus.dinner.splice(i, 1);
-        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`
+        return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`;
       }
     }
-    return `Sorry, we don't sell ${item}, try adding a new recipe!`
+    return `Sorry, we don't sell ${item}, try adding a new recipe!`;
   }
 }
 
