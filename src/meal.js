573e5bf2c642008f42de4afe2dda01b06ec04004
mcraig2342 <matthewcookecraig10@gmail.com>
2021-02-07 12:10:40 -0700

    Finish meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+}
 
+function createMenuItem(name, price, type) {
+var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+}
+
+function addIngredients(newIngredient, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (newIngredient === ingredients[i]) {
+      return ingredients;
+    }
+  }
+    ingredients.push(newIngredient);
+    return ingredients;
+  }
+
+function formatPrice(initialPrice){
+    return "$" + initialPrice;
+  }
+
+function decreasePrice(price) {
+var decreasedPrice = price * .9;
+return decreasedPrice;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
